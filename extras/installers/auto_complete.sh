4013a8755e272840c1002aa8391313abb8ef1c0c
brockar <martinnguzman.mg@gmail.com>
2024-02-27 14:23:30 -0300

    gnome-extensions

---
+++ extras/installers/auto_complete.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://github.com/dvorka/hstr?tab=readme-ov-file
+#Check if user is root
+if [ $(id -u) -ne 0 ]; then
+	echo "Please run as root"
+	exit
+fi
+sudo apt install hstr
+hstr --show-bash-configuration >>~/.bashrc
