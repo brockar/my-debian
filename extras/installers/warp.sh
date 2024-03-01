3fc37d5c8576ff6b637988503ccbb9d8ed3141db
brockar <martinnguzman.mg@gmail.com>
2024-03-01 18:23:08 -0300

    warp modify

--- extras/installers/warp.sh
+++ extras/installers/warp.sh
@@ -6,3 +6,4 @@ if [ $(id -u) -ne 0 ]; then
 fi
 wget -NO warp.deb https://releases.warp.dev/stable/v0.2024.02.20.08.01.stable_02/warp-terminal_0.2024.02.20.08.01.stable.02_amd64.deb
 sudo nala install ./warp.deb -y
+rm warp.deb
