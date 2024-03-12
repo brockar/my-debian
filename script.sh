03cae55991c06c57926b0ed2fea0c5d82dd62353
brockar <martinnguzman.mg@gmail.com>
2024-03-12 12:33:32 -0300

    fix something

--- script.sh
+++ script.sh
@@ -35,23 +35,23 @@ sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flat
 
 #############################################################################################################
 ### CLEAN
-echo -e "\n\n\nRemove some default apps and games?\n\n1. YES \n2. NO [Default]\n"
+echo -e "\n\n\nRemove some default apps and games?\n\n1. YES [Default]\\n2. NO n"
 read respuesta
-if [ "$respuesta" -eq 1 ]; then
+if [ "$respuesta" -eq 2 ]; then
+	echo "no app is being removed. xd"
+else
 	echo "Removing apps..."
 	##games
 	#sudo nala remove gnome-mines gnome-robots gnome-sudoku gnome-taquin gnome-tetravex chees egnome-2048 gnome-klotski gnome-mahjongg gnome-nibbles gnome-chess swell-foop tali five-or-more four-in-a-row aisleriot hitori lightsoff quadrapassel -y
 	sudo nala remove gnome-games -y
-	sudo nala autoremove
 	##gnome software
 	sudo nala remove gnome-sound-recorder gnome-text-editor gnome-maps shotwell sane-airscan evolution rhythmbox transmission-common transmission transmission-gtk totem gnome-music -y
-else
-	echo "no app is being removed. xd"
+	sudo nala autoremove -y
 fi
 
 #############################################################################################################
 ### Install Apps
-sudo nala update && sudo nala upgrade
+sudo nala update && sudo nala upgrade -y
 sudo nala install wget gpg curl rclone thunderbird zoxide trash-cli preload timeshift gparted gnome-shell-extension-manager htop btop tree tldr audacity helvum -y
 flatpak install flathub md.obsidian.Obsidian com.discordapp.Discord com.prusa3d.PrusaSlicer io.github.vikdevelop.SaveDesktop com.rtosta.zapzap org.videolan.VLC -y
 
