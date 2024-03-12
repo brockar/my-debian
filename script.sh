ff417ac8f3c5ebbe0bd17bf8d3bb3bfa93b69872
brockar <martinnguzman.mg@gmail.com>
2024-03-11 21:49:58 -0300

    nala -y

--- script.sh
+++ script.sh
@@ -49,14 +49,14 @@ sudo nala install htop wget gpg rclone thunderbird zoxide trash-cli preload time
 flatpak install flathub md.obsidian.Obsidian com.discordapp.Discord com.prusa3d.PrusaSlicer io.github.vikdevelop.SaveDesktop com.rtosta.zapzap org.videolan.VLC -y
 
 ## VS Code
-sudo nala install wget gpg curl
+sudo nala install wget gpg curl -y
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo nala install apt-transport-https
+sudo nala install apt-transport-https -y
 sudo nala update
-sudo nala install code
+sudo nala install code -y
 
 ## Firefox
 sudo nala purge firefox-esr* -y
