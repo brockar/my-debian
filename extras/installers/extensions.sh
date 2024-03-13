bec753f0b16ae852e2150c37addb3526043a3c85
brockar <martinnguzman.mg@gmail.com>
2024-03-13 16:37:51 -0300

    Gnome Extensions

---
+++ extras/installers/extensions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+EXT_LIST=(
+	blur-my-shell@aunetx
+	dash-to-dock@micxgx.gmail.com
+	clipboard-indicator@tudmotu.com
+	freon@UshakovVasilii_Github.yahoo.com
+	trayIconsReloaded@selfmade.pl
+	gnome-ui-tune@itstime.tech
+	emoji-copy@felipeftn
+)
+
+for i in "${EXT_LIST[@]}"; do
+	busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${i}
+done
+
+gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
