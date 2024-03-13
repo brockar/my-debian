0e1ba2f1feaf6d342f3feb20c2031c8c96205d3f
brockar <martinnguzman.mg@gmail.com>
2024-03-13 08:29:16 -0300

    nvim fix

--- extras/installers/nvim.sh
+++ extras/installers/nvim.sh
@@ -4,11 +4,16 @@ if [ $(id -u) -ne 0 ]; then
 	echo "Please run as root"
 	exit
 fi
+
+echo "LAZYVIM"
+usr=$(id -u -n 1000)
+dir=$(pwd)
+home=/home/$usr
 #############################################################################################################
 ## NVIM
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-sudo ln -s nvim.appimage /usr/local/bin/nvim
+chmod u+x $dir/nvim.appimage
+sudo ln -s $dir/nvim.appimage /usr/local/bin/nvim
 #sudo mv $dir/nvim.appimage /usr/local/bin/nvim
 
 # ## LazyVIM
@@ -24,9 +29,10 @@ rm -rf $home/.config/nvim/.git
 ## Clipboard for lazyVim
 sudo nala install xclip -y
 ## Lazy git
-cd $dir
+
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+
 tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+sudo install $dir/lazygit /usr/local/bin
 rm lazygit.tar.gz
