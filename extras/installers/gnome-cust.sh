4060ca5594aa6e5368c81aea7e68d909871cf3bb
brockar <martinnguzman.mg@gmail.com>
2024-03-14 17:39:51 -0300

    gnome-settings-try1

--- extras/installers/gnome-cust.sh
+++ extras/installers/gnome-cust.sh
@@ -8,14 +8,14 @@ gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'inter
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
 gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
 gsettings set org.gnome.desktop.wm.preferences action-middle-click-titlebar 'minimize'
+gsettings set org.gnome.settings-daemon.plugins.media-keys www ['<Super>w']
 #Doesn't work create a custom shortcut 
-#gsettings set org.gnome.desktop.wm.keybindings activate-window-menu @as [] 
-#gsettings set org.gnome.desktop.wm.keybindings switch-applications '<Super>Tab'
-#gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward ['<Shift><Super>Tab']
-#gsettings set org.gnome.desktop.wm.keybindings switch-windows ['<Alt>Tab']
-#gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward ['<Shift><Alt>Tab']
-#gsettings set org.gnome.settings-daemon.plugins.media-keys control-center ['<Super>i']
-#gsettings set org.gnome.settings-daemon.plugins.media-keys home ['<Super>e']
-#gsettings set org.gnome.settings-daemon.plugins.media-keys search ['<Alt>space']
-#gsettings set org.gnome.settings-daemon.plugins.media-keys www ['<Super>w']
-#gsettings set org.gnome.shell.keybindings toggle-message-tray @as []
+gsettings set org.gnome.desktop.wm.keybindings activate-window-menu "@as []" 
+gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
+gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys control-center "['<Super>i']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys search "['<Alt>space']"
+gsettings set org.gnome.shell.keybindings toggle-message-tray @as "[]"
