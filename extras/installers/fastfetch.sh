70309f6701e07c4277b5b1c9dd12354ea12b5703
brockar <martinnguzman.mg@gmail.com>
2024-05-02 16:05:41 -0300

    FastFetch

--- extras/installers/fastfetch.sh
+++ extras/installers/fastfetch.sh
@@ -4,6 +4,6 @@ if [ $(id -u) -ne 0]; then
 	exit
 fi
 
-wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.8.3/fastfetch-linux-x86_64.deb
+wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.11.1/fastfetch-linux-x86_64.deb
 sudo apt install ./fastfetch-linux-x86_64.deb
 sudo rm ./fastfetch-linux-x86_64.deb
