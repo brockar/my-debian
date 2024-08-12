19048848cbd222f888032f99393b7173f489e4e6
brockar <martinnguzman.mg@gmail.com>
2024-08-12 01:35:10 -0300

    nala

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Check if user is root
 if [ $(id -u) -ne 0 ]; then
-	echo "Please run as root"
-	exit
+  echo "Please run as root"
+  exit
 fi
 #vars
 usr=$(id -u -n 1000)
@@ -30,14 +30,14 @@ sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flat
 echo -e "\n\n\nRemove some default apps and games?\n\n1. YES [Default]\\n2. NO \n"
 read respuesta
 if [ "$respuesta" -eq 2 ]; then
-	echo "no app is being removed. xd"
+  echo "no app is being removed. xd"
 else
-	echo "Removing apps..."
-	##games
-	sudo nala remove gnome-games -y
-	##gnome software
-	sudo nala remove gnome-sound-recorder gnome-text-editor gnome-maps shotwell sane-airscan evolution rhythmbox transmission totem gnome-music -y #transmission-gtk transmission-common
-	sudo nala autoremove -y
+  echo "Removing apps..."
+  ##games
+  sudo nala remove gnome-games -y
+  ##gnome software
+  sudo nala remove gnome-sound-recorder gnome-text-editor gnome-maps shotwell sane-airscan evolution rhythmbox transmission totem gnome-music -y #transmission-gtk transmission-common
+  sudo nala autoremove -y
 fi
 
 #############################################################################################################
@@ -45,7 +45,7 @@ fi
 sudo nala update && sudo nala upgrade -y
 
 for pkg in wget gpg curl rclone thunderbird zoxide trash-cli preload timeshift gparted gnome-shell-extension-manager htop btop tree tldr helvum; do
-	sudo nala install $pkg -y
+  sudo nala install $pkg -y
 done
 
 flatpak install flathub md.obsidian.Obsidian com.discordapp.Discord com.prusa3d.PrusaSlicer io.github.vikdevelop.SaveDesktop com.rtosta.zapzap org.videolan.VLC -y
@@ -63,20 +63,20 @@ $dir/extras/installers/nvim.sh
 echo -e "\n\n\nInstall QEMU? \n\n1. YES \n2. NO [Default]"
 read respuesta
 if [ "$respuesta" -eq 1 ]; then
-	echo "\nInstalling QEMU..."
-	sudo nala install qemu-kvm virt-manager virtinst spice-vdagent libvirt-clients bridge-utils libvirt-daemon-system -y
-	sudo systemctl enable --now libvirtd
-	sudo systemctl start libvirtd
+  echo "\nInstalling QEMU..."
+  sudo nala install qemu-kvm virt-manager virtinst spice-vdagent libvirt-clients bridge-utils libvirt-daemon-system -y
+  sudo systemctl enable --now libvirtd
+  sudo systemctl start libvirtd
 
-	sudo usermod -aG kvm $usr
-	sudo usermod -aG libvirt $usr
+  sudo usermod -aG kvm $usr
+  sudo usermod -aG libvirt $usr
 fi
 
 ## Docker
 echo -e "\n\n\nInstall Docker?\n\n1. YES \n2. NO [Default]\n"
 read respuesta
 if [ "$respuesta" -eq 1 ]; then
-	$dir/extras/installers/docker.sh
+  $dir/extras/installers/docker.sh
 fi
 
 #############################################################################################################
@@ -86,7 +86,6 @@ fi
 #git clone https://github.com/ChrisTitusTech/debian-titus
 $dir/debian-titus/install.sh
 $dir/debian-titus/mybash/setup.sh
-$dir/debian-titus/scripts/usenala
 
 ## Grub Theme
 echo "Install Grub Theme"
